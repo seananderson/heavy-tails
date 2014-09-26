594ce012a17b37d967c7e6f4815a42c0a5e5fb52
Sean Anderson <github@seananderson.ca>
2014-09-26 12:36:16 -0700

    Output more values for the ms

--- 7-values-for-paper.R
+++ 7-values-for-paper.R
@@ -22,7 +22,16 @@ perc_imputed_pops <- as.character(perc_imputed$percent_imputed)
 
 perc_imputed_points <- round(sum(gpdd$interpolated)/nrow(gpdd) * 100, 1)
 
+fig2_pop_n <- filter(gpdd, taxonomic_class %in% c("Aves", "Mammalia", "Insecta", "Osteichthyes")) %>% summarise(n = length(unique(main_id))) %>% summarise(N = sum(n))
+fig2_pop_n <- fig2_pop_n$N
+
+
+temp <- gpdd %>% group_by(main_id) %>% summarise(assume_log10 = assumed_log10[1]) %>% summarise(total_assumed_log10 = sum(assume_log10))
+total_assumed_log10 <- temp$total_assumed_log10
+
 zz <- file("values.tex", "w")
 write_tex(perc_imputed_pops, "percImputedPops")
 write_tex(perc_imputed_points, "percImputedPoints")
+write_tex(fig2_pop_n, "nuCoefPopN")
+write_tex(total_assumed_log10, "totalAssumedLog")
 close(zz)
