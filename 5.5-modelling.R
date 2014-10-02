1ad38c72d8b48b6a0e29e517a40531ecd86e0665
Sean Anderson <github@seananderson.ca>
2014-10-01 17:58:30 -0700

    Clean up the modelling file

--- 5.5-modelling.R
+++ 5.5-modelling.R
@@ -1,4 +1,9 @@
-library(lme4)
+# this file fits GLMMs to the probability of heavy tails
+# to examine correlates
+# note that the glmmADMB models take a while to compile and fit
+# there are remnants of an lme4 analysis commented out
+
+# library(lme4)
 
 #gomp_hat_base$log10_Lifesp_scaled <- scale(gomp_hat_base$log10_Lifesp)
 gomp_hat_base$log_Lifesp_scaled <- scale(log(gomp_hat_base$Lifesp))
@@ -13,7 +18,8 @@ gomp_hat_base$heavy <- ifelse(gomp_hat_base$p10 > 0.5, 1, 0)
 # i <<- 0
 # get_lmer_coefs <- function(param) {
 #   i <<- i + 1
-#   m1 <- glmer(heavy ~ gomp_hat_base[, param] + (1 | taxonomic_class / taxonomic_order), family = binomial, data = gomp_hat_base)
+#   m1 <- glmer(heavy ~ gomp_hat_base[, param] + (1 | taxonomic_class /
+#       taxonomic_order), family = binomial, data = gomp_hat_base)
 #   models[[i]] <- m1
 #   est <- fixef(m1)[[2]]
 #   ci.50 <- tryCatch(confint(m1, parm = 4, level = 0.5), warning = function(w) c(NA, NA))
@@ -46,29 +52,46 @@ gomp_hat_base$heavy <- ifelse(gomp_hat_base$p10 > 0.5, 1, 0)
 # axis(2, at = out$pos, labels = out$predictor, las = 2)
 # abline(v = 0, lty = 2)
 
-#m.all <- glmer(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled + log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = binomial, data = gomp_hat_base)
+#m.all <- glmer(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  #log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
+    #taxonomic_class / taxonomic_order), family = binomial, data = gomp_hat_base)
 
 library(glmmADMB)
-temp.dat <- gomp_hat_base[,c("log_Lifesp_scaled", "log_sigma_proc_50_scaled", "log_dataset_length_scaled", "b_50_scaled", "lambda_50_scaled", "taxonomic_class", "taxonomic_order", "heavy", "p10")]
+temp.dat <- gomp_hat_base[,c("log_Lifesp_scaled", "log_sigma_proc_50_scaled",
+  "log_dataset_length_scaled", "b_50_scaled", "lambda_50_scaled",
+  "taxonomic_class", "taxonomic_order", "heavy", "p10")]
 temp.dat <- na.omit(temp.dat)
 temp.dat$taxonomic_class <- as.factor(temp.dat$taxonomic_class)
 temp.dat$taxonomic_order <- as.factor(temp.dat$taxonomic_order)
-m.admb.1 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled + log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = "binomial", data = temp.dat)
+m.admb.1 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
+    taxonomic_class / taxonomic_order), family = "binomial", data = temp.dat)
 save(m.admb.1, file = "m.admb.1.rda")
 
-m.admb.2 <- glmmadmb(p10 ~ log_Lifesp_scaled + log_sigma_proc_50_scaled + log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = "beta", data = temp.dat)
+m.admb.2 <- glmmadmb(p10 ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
+    taxonomic_class / taxonomic_order), family = "beta", data = temp.dat)
 save(m.admb.2, file = "m.admb.2.rds")
 
-temp.dat2 <- gomp_hat_base[,c("log_Lifesp_scaled", "log_sigma_proc_50_scaled", "dataset_length", "log_dataset_length_scaled", "b_50_scaled", "lambda_50_scaled", "taxonomic_class", "taxonomic_order", "heavy", "p10")]
+temp.dat2 <- gomp_hat_base[,c("log_Lifesp_scaled", "log_sigma_proc_50_scaled",
+  "dataset_length", "log_dataset_length_scaled", "b_50_scaled",
+  "lambda_50_scaled", "taxonomic_class", "taxonomic_order", "heavy", "p10")]
 temp.dat2 <- na.omit(temp.dat2)
 temp.dat2$taxonomic_class <- as.factor(temp.dat2$taxonomic_class)
 temp.dat2$taxonomic_order <- as.factor(temp.dat2$taxonomic_order)
 
-m.admb.3 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled + dataset_length + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = "binomial", data = temp.dat2)
+m.admb.3 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  dataset_length + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class /
+    taxonomic_order), family = "binomial", data = temp.dat2)
+save(m.admb.3, file = "m.admb.3.rds")
 
-m.admb.4 <- glmmadmb(p10 ~ log_Lifesp_scaled + log_sigma_proc_50_scaled + dataset_length + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = "beta", data = temp.dat2)
+m.admb.4 <- glmmadmb(p10 ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  dataset_length + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class /
+    taxonomic_order), family = "beta", data = temp.dat2)
+save(m.admb.4, file = "m.admb.4.rds")
 
-newdat <- data.frame(log_Lifesp_scaled = 0, log_sigma_proc_50_scaled = 0, dataset_length = seq(20, 100), b_50_scaled = 0, lambda_50_scaled = 0)
+newdat <- data.frame(log_Lifesp_scaled = 0, log_sigma_proc_50_scaled = 0,
+  dataset_length = seq(20, 100), b_50_scaled = 0, lambda_50_scaled = 0)
 
 p <- predict(m.admb.3, newdata = newdat, se = TRUE)
 x <- newdat$dataset_length
@@ -76,27 +99,35 @@ p.beta <- predict(m.admb.4, newdata = newdat, se = TRUE)
 
 pdf("length-vs-prob-tails.pdf", width = 7, height = 4)
 par(mfrow = c(1, 2), las = 1)
-par(mar = c(4, 1, 2, 1), cex = 0.8, oma = c(1, 4, 1, 1))
+par(mar = c(4, 1, 2, 1), cex = 0.9, oma = c(1, 4, 1, 1))
 
-plot(x, plogis(p.beta$fit), type = "l", ylab = "", xlab = "Time-series length", ylim = c(0, 1.01), log = "x", yaxs = "i")
-with(temp.dat2, points(dataset_length, p10, pch = 21, col = "#00000070", bg = "#00000030"))
-polygon(c(x, rev(x)), c(plogis(p.beta$fit + qnorm(0.75) * p.beta$se.fit), rev(plogis(p.beta$fit - qnorm(0.75) * p.beta$se.fit))), border = NA, col = "#FF000020")
-polygon(c(x, rev(x)), c(plogis(p.beta$fit + qnorm(0.975) * p.beta$se.fit), rev(plogis(p.beta$fit - qnorm(0.975) * p.beta$se.fit))), border = NA, col = "#FF000020")
+plot(x, plogis(p.beta$fit), type = "l", ylab = "", xlab = "Time-series length",
+  ylim = c(0, 1.01), log = "x", yaxs = "i")
+with(temp.dat2, points(dataset_length, p10, pch = 21, col = "#00000070",
+    bg = "#00000030"))
+polygon(c(x, rev(x)), c(plogis(p.beta$fit + qnorm(0.75) * p.beta$se.fit),
+    rev(plogis(p.beta$fit - qnorm(0.75) * p.beta$se.fit))), border = NA,
+  col = "#FF000020")
+polygon(c(x, rev(x)), c(plogis(p.beta$fit + qnorm(0.975) * p.beta$se.fit),
+    rev(plogis(p.beta$fit - qnorm(0.975) * p.beta$se.fit))), border = NA,
+  col = "#FF000020")
 par(xpd = NA)
-mtext("Probability of\nobserving heavy tails", side = 2, line = 2.5, cex = 0.8, las = 0)
+mtext("Probability of\nobserving heavy tails", side = 2, line = 2.5, cex = 0.8,
+  las = 0)
 mtext("Beta model", side = 3, line = 1, cex = 0.8, las = 1)
 par(xpd = FALSE)
 
-plot(x, plogis(p$fit), type = "l", ylab = "", xlab = "Time-series length", ylim = c(0, 1), log = "x", yaxt = "n")
-with(temp.dat2, points(dataset_length, jitter(heavy, 0.1), pch = 21, col = "#00000070", bg = "#00000030"))
-polygon(c(x, rev(x)), c(plogis(p$fit + qnorm(0.75) * p$se.fit), rev(plogis(p$fit - qnorm(0.75) * p$se.fit))), border = NA, col = "#FF000020")
-polygon(c(x, rev(x)), c(plogis(p$fit + qnorm(0.975) * p$se.fit), rev(plogis(p$fit - qnorm(0.975) * p$se.fit))), border = NA, col = "#FF000020")
+plot(x, plogis(p$fit), type = "l", ylab = "", xlab = "Time-series length", ylim
+  = c(0, 1), log = "x", yaxt = "n")
+with(temp.dat2, points(dataset_length, jitter(heavy, 0.1), pch = 21, col =
+    "#00000070", bg = "#00000030"))
+polygon(c(x, rev(x)), c(plogis(p$fit + qnorm(0.75) * p$se.fit), rev(plogis(p$fit
+        - qnorm(0.75) * p$se.fit))), border = NA, col = "#FF000020")
+polygon(c(x, rev(x)), c(plogis(p$fit + qnorm(0.975) * p$se.fit),
+    rev(plogis(p$fit - qnorm(0.975) * p$se.fit))), border = NA, col =
+  "#FF000020")
 mtext("Binomial model", side = 3, line = 1, cex = 0.8, las = 1)
 dev.off()
-
-
-
-
 
 #est.lmer.bin <- fixef(m.all)
 est.admb.bin <- fixef(m.admb.1)
@@ -139,10 +170,12 @@ make_coef_plot(ci.admb.bin.50, ci.admb.bin.95, est.admb.bin)
 mtext("Binomial model\ncoefficient", side = 1, line = 3)
 dev.off()
 
-#m.all2 <- glmer(heavy ~ log10_Len_scaled + log_sigma_proc_50_scaled + log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class / taxonomic_order), family = binomial, data = gomp_hat_base)
-#
+#m.all2 <- glmer(heavy ~ log10_Len_scaled + log_sigma_proc_50_scaled +
+  #log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
+    #taxonomic_class / taxonomic_order), family = binomial, data = gomp_hat_base)
+
 # test for overdispersion, function from http://glmm.wikidot.com/faq
-#
+
 overdisp_fun <- function(model) {
   ## number of variance parameters in
   ##   an n-by-n variance-covariance matrix
@@ -161,13 +194,10 @@ overdisp_fun(m.admb.1)
 overdisp_fun(m.admb.2)
 #overdisp_fun(m.all)
 
-#est <- fixef(m.all)
-#ci.95 <- confint(m.all, level = 0.95)
-
-#ci.50 <- confint(m.all, level = 0.50)
-
-# get_lmer_coefs("sigma_proc_50_scaled")
-# get_lmer_coefs("b_50_scaled")
-# get_lmer_coefs("lambda_50_scaled")
-# get_lmer_coefs("dataset_length_scaled")
-# get_lmer_coefs("log10_Len_scaled")
+# MCMC: (takes a long time, and needs more iterations than this)
+#m.admb.5 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  #dataset_length + b_50_scaled + lambda_50_scaled + (1 | taxonomic_class /
+    #taxonomic_order), family = "binomial", data = temp.dat2, mcmc = TRUE)
+#m <- as.mcmc(m.admb.5$mcmc)
+#library(scapeMCMC)
+#plotTrace(m)
