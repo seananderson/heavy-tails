c43ddb6512f99d59cd4bce5de0edf58c07a4afb4
Sean Anderson <github@seananderson.ca>
2014-10-06 14:12:26 -0700

    Add preliminary modelling work
    Not using this, but preserving

--- 5.5-modelling-IGNORE.R
+++ 5.5-modelling-IGNORE.R
@@ -70,8 +70,9 @@ save(m.admb.1, file = "m.admb.1.rda")
 
 m.admb.2 <- glmmadmb(p10 ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
   log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
-    taxonomic_class / taxonomic_order), family = "beta", data = temp.dat)
-save(m.admb.2, file = "m.admb.2.rds")
+    taxonomic_class / taxonomic_order), family = "beta", data = temp.dat,
+  save.dir = "admb")
+save(m.admb.2, file = "m.admb.2.rda")
 
 temp.dat2 <- gomp_hat_base[,c("log_Lifesp_scaled", "log_sigma_proc_50_scaled",
   "dataset_length", "log_dataset_length_scaled", "b_50_scaled",
@@ -201,3 +202,92 @@ overdisp_fun(m.admb.2)
 #m <- as.mcmc(m.admb.5$mcmc)
 #library(scapeMCMC)
 #plotTrace(m)
+#
+#
+# now try with Stan:
+
+# stan_model <-
+# "data {
+#   int<lower=1> N; // number of data points
+#   //int<lower=1> n_class; // number of classes
+#   int<lower=1> n_order; // number of orders
+#   vector<lower=0,upper=1>[N] y; // response data
+#   vector[N] x; // predictor vector
+#   //int<lower=1,upper=n_class> class_id[N];
+#   int<lower=1,upper=n_order> order_id[N];
+# }
+# parameters {
+#   //vector[n_class] beta_class;
+#   vector[n_order] beta_order;
+#   //real mu_beta1;
+#   //real<lower=0> tau_class; // sd on class-level intercept
+#   real<lower=0> tau_order; // sd on order-level intercept
+#   real mu_beta0; // global intercept
+#   real beta1; // global slope
+# }
+# transformed parameters {
+#   vector[N] y_hat;
+#   for(i in 1:N) {
+#     //y_hat[i] <- mu_beta0 + beta_order[order_id[i]] + x[i] * beta1;
+#     y_hat[i] <- mu_beta0 + x[i] * beta1;
+#   }
+# }
+# model {
+#   mu_beta0 ~ normal(0, 10);
+#   //tau_class ~ cauchy(0, 5);
+#   tau_order ~ cauchy(0, 5);
+# //  beta_class ~ normal(0, 10);
+#   beta_order ~ normal(0, 10);
+#   beta1 ~ normal(0, 10);
+#   y ~ binomial_logit(y_hat);
+# }
+# "
+
+stan_model <-
+"data {
+  int<lower=0> N;
+  int<lower=0> n_order;
+  vector[N] x;
+  int<lower=1,upper=n_order> order[N];
+  int<lower=0,upper=1> y[N];
+}
+parameters {
+  vector[n_order] a;
+  real b;
+  real mu_a;
+  real sigma_a;
+}
+transformed parameters {
+  vector[N] y_hat;
+  for (i in 1:N)
+    y_hat[i] <- b * x[i] + a[order[i]];
+}
+model {
+  mu_a ~ normal(0, 1);
+  a ~ normal(100 * mu_a, sigma_a);
+  b ~ normal(0, 10);
+  sigma_a ~ cauchy(0, 2.5);
+  y ~ bernoulli_logit(y_hat);
+}"
+
+library(rstan)
+stan_logistic <- stan_model(model_code = stan_model)
+
+#m.admb.1 <- glmmadmb(heavy ~ log_Lifesp_scaled + log_sigma_proc_50_scaled +
+  #log_dataset_length_scaled + b_50_scaled + lambda_50_scaled + (1 |
+    #taxonomic_class / taxonomic_order), family = "binomial", data = temp.dat)
+
+d <- temp.dat[sample(1:nrow(temp.dat), 100), ]
+d$order_id <- as.numeric(d$taxonomic_order)
+
+m.lmer.1 <- glmer(heavy ~ log_dataset_length_scaled + (1 | taxonomic_order), family = "binomial", data = d)
+
+m.stan.1 <- sampling(stan_logistic,
+  data = list(
+    N = nrow(d),
+    n_order = max(d$order_id),
+    x = as.numeric(d$log_dataset_length_scaled),
+    order = d$order_id,
+    y = d$heavy),
+  pars = c("b", "mu_a", "sigma_a"),
+  iter = 1000, chains = 3)
