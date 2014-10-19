c10876ed30423d791970617cf69aa3a39cb6df0a
Sean Anderson <github@seananderson.ca>
2014-10-18 18:08:25 -0700

    Delete file that we're not using

--- 6-plot-p20-order.R
+++
@@ -1,8 +0,0 @@
-# make a publication plot of p(nu < 20) by taxonomic order and class
-
-# plot_nu_panel <- function
-
-x <- filter(gomp_hat_ar1, taxonomic_class == "Mammalia") %>%
-  arrange(taxonomic_class, order_p20) %>%
-
-
