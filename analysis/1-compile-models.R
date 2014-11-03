ae8b82144723b726184341c5452cfba9454c48bf
Sean Anderson <github@seananderson.ca>
2014-11-02 17:01:15 -0800

    Clean out some Stan models I'm not using

--- analysis/1-compile-models.R
+++ analysis/1-compile-models.R
@@ -61,42 +61,6 @@ model {
 stan_gomp_ar1 <- stan_model(model_code = stan_model)
 saveRDS(stan_gomp_ar1, file = "stan-gomp-ar1.rds")
 
-# with fixed observation error:
-
-stan_model <-
-  'data {
-  int<lower=0> N; // rows of data
-  vector[N] y; // vector to hold observations
-  real<lower=0> nu_rate; // rate parameter for nu exponential prior
-  real b_lower;
-  real b_upper;
-  real<lower=0> sigma_obs;
-}
-parameters {
-  real lambda;
-  real<lower=b_lower, upper=b_upper> b;
-  real<lower=0> sigma_proc;
-  real<lower=2> nu;
-  real<lower=-1, upper=1> phi;
-  vector[N] U; // states
-}
-model {
-  nu ~ exponential(nu_rate);
-  lambda ~ normal(0, 10);
-  sigma_proc ~ cauchy(0, 2.5);
-  phi ~ normal(0, 1);
-
-  for (i in 3:N) {
-    U[i] ~ student_t(nu,
-            (lambda + b * U[i-1]) - phi * (lambda + b * U[i-2]),
-            sigma_proc);
-  }
-  y ~ normal(U, sigma_obs);
-}
-'
-stan_gomp_ar1_obs <- stan_model(model_code = stan_model)
-saveRDS(stan_gomp_ar1_obs, file = "stan-gomp-ar1-obs.rds")
-
 # with fixed observation error (and no autocorrelation):
 
 stan_model <-
@@ -179,50 +143,6 @@ model {
 stan_logistic <- stan_model(model_code = stan_model)
 saveRDS(stan_logistic, file = "stan-logistic.rds")
 
-# Ricker-logistic model
-# with AR1 residuals
-
-stan_model <-
-'data {
-  int<lower=0> N; // rows of data
-  vector[N] y; // vector to hold observations
-  real<lower=0> nu_rate; // rate parameter for nu exponential prior
-  real<lower=0> K_upper;
-  real<lower=0> r_upper;
-}
-parameters {
-  real lambda;
-  real<lower=0, upper=K_upper> K;
-  real<lower=0, upper=r_upper> r;
-  real<lower=0> sigma_proc;
-  real<lower=2> nu;
-  real<lower=-1, upper=1> phi;
-}
-transformed parameters {
-  vector[N] exp_y;      // abundance on non-logged scale
-  vector[N] epsilon;    // error terms
-  exp_y <- exp(y);
-  epsilon[1] <- 0;
-  for (i in 2:N) {
-    epsilon[i] <- y[i] - (r * (1 - (exp_y[i - 1])/K))
-                       - (phi * epsilon[i - 1]);
-  }
-}
-model {
-  nu ~ exponential(nu_rate);
-  sigma_proc ~ cauchy(0, 2.5);
-  phi ~ normal(0, 1);
-  for (i in 2:N) {
-    y[i] ~ student_t(nu,
-                     y[i - 1] + r * (1 - (exp_y[i - 1]/K))
-                     + phi * epsilon[i - 1],
-                     sigma_proc);
-  }
-}
-'
-stan_logistic_ar1 <- stan_model(model_code = stan_model)
-saveRDS(stan_logistic_ar1, file = "stan-logistic-ar1.rds")
-
 # AR1 Gompertz that has been extensively tested
 # coded similarly to the Stan 2.4.0 manual
 stan_model <-
@@ -264,137 +184,6 @@ model {
 stan_gomp2_ar1 <- stan_model(model_code = stan_model)
 saveRDS(stan_gomp2_ar1, file = "stan-gomp2-ar1.rds")
 
-# and the same AR1 Gompertz model with specified observation error:
-
-stan_model <-
-'data {
-  int<lower=0> N; // rows of data
-  vector[N] y; // vector to hold observations
-  real<lower=0> nu_rate; // rate parameter for nu exponential prior
-  real b_lower;
-  real b_upper;
-  real<lower=0> sigma_obs;
-}
-parameters {
-  real lambda;
-  real<lower=b_lower, upper=b_upper> b;
-  real<lower=0> sigma_proc;
-  real<lower=2> nu;
-  real<lower=-1, upper=1> phi;
-  vector[N] U; // states
-}
-transformed parameters {
-  vector[N] epsilon;    // error terms
-  epsilon[1] <- 0;
-  for (i in 2:N) {
-    epsilon[i] <- U[i] - (lambda + b * U[i - 1]) -
-      (phi * epsilon[i - 1]);
-  }
-}
-model {
-  nu ~ exponential(nu_rate);
-  lambda ~ normal(0, 10);
-  sigma_proc ~ cauchy(0, 2.5);
-  phi ~ normal(0, 1);
-  for (i in 2:N) {
-    U[i] ~ student_t(nu,
-      lambda + b * U[i - 1]
-      + phi * epsilon[i - 1],
-      sigma_proc);
-  }
-  y ~ normal(U, sigma_obs);
-}'
-stan_gomp2_ar1_obs <- stan_model(model_code = stan_model)
-saveRDS(stan_gomp2_ar1_obs, file = "stan-gomp2-ar1-obs.rds")
-
-# and the same but estimating obs. error quantity:
-
-stan_model <-
-  'data {
-int<lower=0> N; // rows of data
-vector[N] y; // vector to hold observations
-real<lower=0> nu_rate; // rate parameter for nu exponential prior
-real b_lower;
-real b_upper;
-//real<lower=0> sigma_obs;
-}
-parameters {
-real lambda;
-real<lower=b_lower, upper=b_upper> b;
-real<lower=0> sigma_proc;
-real<lower=0> sigma_obs;
-real<lower=2> nu;
-real<lower=-1, upper=1> phi;
-vector[N] U; // states
-}
-transformed parameters {
-vector[N] epsilon;    // error terms
-epsilon[1] <- 0;
-for (i in 2:N) {
-epsilon[i] <- U[i] - (lambda + b * U[i - 1]) -
-(phi * epsilon[i - 1]);
-}
-}
-model {
-nu ~ exponential(nu_rate);
-lambda ~ normal(0, 10);
-sigma_proc ~ cauchy(0, 2.5);
-sigma_obs ~ cauchy(0, 2.5);
-phi ~ normal(0, 1);
-for (i in 2:N) {
-U[i] ~ student_t(nu,
-lambda + b * U[i - 1]
-+ phi * epsilon[i - 1],
-sigma_proc);
-}
-y ~ normal(U, sigma_obs);
-}'
-stan_gomp2_ar1_obs_est <- stan_model(model_code = stan_model)
-saveRDS(stan_gomp2_ar1_obs_est, file = "stan-gomp2-ar1-obs-est.rds")
-
-
-# and assume that obs. and proc. error sd are the same:
-# (even though the variable name is sigma_proc)
-stan_model <-
-'data {
-  int<lower=0> N; // rows of data
-  vector[N] y; // vector to hold observations
-  real<lower=0> nu_rate; // rate parameter for nu exponential prior
-  real b_lower;
-  real b_upper;
-}
-parameters {
-  real lambda;
-  real<lower=b_lower, upper=b_upper> b;
-  real<lower=0> sigma_proc;
-  real<lower=2> nu;
-  real<lower=-1, upper=1> phi;
-  vector[N] U; // states
-}
-transformed parameters {
-  vector[N] epsilon;    // error terms
-  epsilon[1] <- 0;
-  for (i in 2:N) {
-    epsilon[i] <- U[i] - (lambda + b * U[i - 1]) -
-    (phi * epsilon[i - 1]);
-  }
-}
-model {
-  nu ~ exponential(nu_rate);
-  lambda ~ normal(0, 10);
-  sigma_proc ~ cauchy(0, 2.5);
-  phi ~ normal(0, 1);
-  for (i in 2:N) {
-    U[i] ~ student_t(nu,
-      lambda + b * U[i - 1]
-      + phi * epsilon[i - 1],
-      sigma_proc);
-  }
-  y ~ normal(U, sigma_proc);
-}'
-stan_gomp2_ar1_obs_equal <- stan_model(model_code = stan_model)
-saveRDS(stan_gomp2_ar1_obs_equal, file = "stan-gomp2-ar1-obs-equal.rds")
-
 # and a basic model of just the growth rates
 # same as setting b = 1 in Gompertz
 
@@ -419,36 +208,6 @@ model {
 
 stan_rate <- stan_model(model_code = stan_model)
 saveRDS(stan_rate, file = "stan-rate.rds")
-
-# equal process and error without any autocorrelation:
-# (even though the variable name is sigma_proc)
-stan_model <-
-'data {
-  int<lower=0> N; // rows of data
-  vector[N] y; // vector to hold observations
-  real<lower=0> nu_rate; // rate parameter for nu exponential prior
-  real b_lower;
-  real b_upper;
-}
-parameters {
-  real lambda;
-  real<lower=b_lower, upper=b_upper> b;
-  real<lower=0> sigma_proc;
-  real<lower=2> nu;
-  vector[N] U; // states
-}
-model {
-  nu ~ exponential(nu_rate);
-  lambda ~ normal(0, 10);
-  sigma_proc ~ cauchy(0, 2.5);
-  for (i in 2:N) {
-    U[i] ~ student_t(nu, lambda + b * U[i - 1], sigma_proc);
-  }
-  y ~ normal(U, sigma_proc);
-}'
-stan_gomp_obs_equal <- stan_model(model_code = stan_model)
-saveRDS(stan_gomp_obs_equal, file = "stan-gomp-obs-equal.rds")
-
 
 # uniform prior on sigma:
 
