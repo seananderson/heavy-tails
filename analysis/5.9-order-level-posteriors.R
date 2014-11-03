002ff8849824fcfb011bf3f2e8e2972c64f11602
Sean Anderson <github@seananderson.ca>
2014-11-02 23:09:08 -0800

    Clean up order-level posteriors plot

--- analysis/5.9-order-level-posteriors.R
+++ analysis/5.9-order-level-posteriors.R
@@ -1,7 +1,6 @@
 # plot order-level posterior predictions
 # start by running 5.8...
 
-
 library("rstan")
 library("ggplot2")
 
@@ -55,10 +54,7 @@ a_df <- droplevels(a_df)
 a_df$sorted_order <- factor(a_df$taxonomic_order,
   levels(a_df$taxonomic_order)[rev(order(a_df$invlogit_a))])
 
-#library(ggplot2)
-#ggplot(a_df, aes(sorted_order, invlogit_a, colour = taxonomic_class, size = n_pops)) + geom_point() + coord_flip() + scale_size(trans = "log10") + xlab("") + ylab(quote(Pr(nu<10)))
-
-########
+###
 mu_a <- extract(m, pars = "mu_a")[[1]]
 a_class <- extract(m, pars = "a_class")[[1]]
 a_order <- extract(m, pars = "a_order")[[1]]
@@ -91,32 +87,30 @@ x <- x[x > 2]
 prior_p10 <- length(x[x < 10])/length(x)
 
 pdf("order-level-estimates.pdf", width = 2.6, height = 10.5)
-par(mfrow = c(length(unique(a_df$order_id)), 1), cex = 0.8, mar = c(0,0,0,0), oma = c(3.5, 3, .5, .5), tcl = -0.05, mgp = c(2, 0.4, 0))
+par(mfrow = c(length(unique(a_df$order_id)), 1), cex = 0.8, mar = c(0,0,0,0),
+  oma = c(3.5, 3, .5, .5), tcl = -0.05, mgp = c(2, 0.4, 0))
 for(i in a_df$order_id) {
   i_order <- i
   i_class <- unique(lu[lu$order_id == i, "class_id"])
   a <- plogis(mu_a + a_order[, i_order] + a_class[,i_class])
-  print(median(a))
   den <- density(a)
-  plot(1, 1, type = "n", xlim = c(0.05, 0.18), ylim = c(0, 42), xaxt = "n", yaxt = "n", axes = FALSE, main = "", yaxs = "i")
+  plot(1, 1, type = "n", xlim = c(0.05, 0.18), ylim = c(0, 42), xaxt = "n",
+    yaxt = "n", axes = FALSE, main = "", yaxs = "i")
   polygon(c(den$x, rev(den$x)), c(den$y, rep(0, length(den$y))),
     border = "grey60", lwd = 2, col = unique(lu[lu$order_id == i, "col"]))
-    #col = "grey85", lwd = 3, border = unique(lu[lu$order_id == i, "col"]))
-  #polygon(c(den$x, rev(den$x)), c(den$y, rep(0, length(den$y))),
-    #col = "grey85", lwd = 0.5, border = "grey10")
   segments(median(a), 0, median(a), 22, col = "grey60", lwd = 1.4)
-  #segments(mean(a), 0, mean(a), 20, col = "#00000040", lwd = 1.4, lty = 2)
   abline(v = prior_p10, lty = 2, col = "grey50", lwd = 0.6)
 
   box(col =  "grey80", lwd = 0.9)
   par(xpd = NA)
-  #axis(2, at = 15, labels = lu[lu$order_id == i, "taxonomic_order"], las = 1, tick = FALSE, line = 0, col.axis = "grey20")
-  legend("topleft", legend = lu[lu$order_id == i, "taxonomic_order"], bty = "n", inset = c(-0.1, -0.15))
-  #legend("topleft", legend = lu[lu$order_id == i, "order_id"], bty = "n", inset = c(-0.1, -0.1))
+  legend("topleft", legend = lu[lu$order_id == i, "taxonomic_order"], bty = "n",
+    inset = c(-0.1, -0.15))
   par(xpd = FALSE)
 }
 axis(1, at = c(0, 0.05, 0.1, 0.15, 0.2), col.axis = "grey20", col = "grey50")
   par(xpd = NA)
-mtext("Posterior probability density", side = 2, line  = 0.5, outer = TRUE, las = 0, col = "grey20", cex = 0.9)
-mtext(quote(Pr(nu<10)), side = 1, line  = 2.3, outer = TRUE, las = 0, col = "grey20", cex = 0.9)
+mtext("Posterior probability density", side = 2, line  = 0.5, outer = TRUE,
+  las = 0, col = "grey20", cex = 0.9)
+mtext(quote(Pr(nu<10)), side = 1, line  = 2.3, outer = TRUE, las = 0,
+  col = "grey20", cex = 0.9)
 dev.off()
