56c91a223754b87f809ea3df470f12621067ae1d
Sean Anderson <github@seananderson.ca>
2014-11-05 14:05:46 -0800

    Add weaker prior model run

---
+++ analysis/2-run-models-base-weaker-prior.R
@@ -0,0 +1,20 @@
+# This file runs the Gompertz models with an AR1 coefficient on the residuals
+
+source("1.5-compile-fit-function.R")
+source("1.6-extract-function.R")
+
+model <- readRDS("stan-gomp.rds")
+gpdd <- readRDS("gpdd-clean.rds")
+id <- "gomp-base"
+
+stan_pars <- c("lambda", "sigma_proc", "nu", "b")
+
+fit_gpdd_model(gpdd_dat = gpdd, model = model, sub_folder = id,
+  pars = stan_pars,
+  stan_dat = paste0("list(N = nrow(x), y = log(x$population_untransformed), ",
+    "nu_rate = 0.005, b_lower = -1, b_upper = 2)"), .parallel = FALSE)
+
+out <- plyr::ldply(unique(gpdd$main_id), extract_model,
+  sub_folder = id, get_phi = FALSE, type = "gompertz")
+
+saveRDS(out, file = paste0(id, "-hat.rds"))
