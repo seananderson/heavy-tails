413988f3038a1a8fa80080e7a55c73d2a7f3eeaa
Sean Anderson <github@seananderson.ca>
2014-11-05 22:13:13 -0800

    Add a file to run a stronger exponential nu prior

---
+++ analysis/2-run-models-base-stronger-prior.R
@@ -0,0 +1,24 @@
+# This file runs the Gompertz models with a more informative prior on nu that
+# favours lower values
+
+source("1.5-compile-fit-function.R")
+source("1.6-extract-function.R")
+
+model <- readRDS("stan-gomp-bda.rds")
+model <- readRDS("stan-gomp.rds")
+gpdd <- readRDS("gpdd-clean.rds")
+id <- "gomp-base-stronger"
+
+# gpdd <- subset(gpdd, main_id %in% c(1))
+
+stan_pars <- c("lambda", "sigma_proc", "nu", "b")
+
+fit_gpdd_model(gpdd_dat = gpdd, model = model, sub_folder = id,
+  pars = stan_pars,
+  stan_dat = paste0("list(N = nrow(x), y = log(x$population_untransformed), ",
+    "nu_rate = 0.05, b_lower = -1, b_upper = 2)"), .parallel = FALSE, cores = 1)
+
+out <- plyr::ldply(unique(gpdd$main_id), extract_model,
+  sub_folder = id, get_phi = FALSE, type = "gompertz")
+
+saveRDS(out, file = paste0(id, "-hat.rds"))
