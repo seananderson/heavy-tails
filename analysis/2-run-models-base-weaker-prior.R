9c2f33babe3a9ad1fab1adfebb746b32d50cb277
Sean Anderson <github@seananderson.ca>
2014-11-05 21:26:10 -0800

    Tweak weaker base model code

--- analysis/2-run-models-base-weaker-prior.R
+++ analysis/2-run-models-base-weaker-prior.R
@@ -5,14 +5,14 @@ source("1.6-extract-function.R")
 
 model <- readRDS("stan-gomp.rds")
 gpdd <- readRDS("gpdd-clean.rds")
-id <- "gomp-base"
+id <- "gomp-base-weaker"
 
 stan_pars <- c("lambda", "sigma_proc", "nu", "b")
 
 fit_gpdd_model(gpdd_dat = gpdd, model = model, sub_folder = id,
   pars = stan_pars,
   stan_dat = paste0("list(N = nrow(x), y = log(x$population_untransformed), ",
-    "nu_rate = 0.005, b_lower = -1, b_upper = 2)"), .parallel = TRUE)
+    "nu_rate = 0.005, b_lower = -1, b_upper = 2)"), .parallel = TRUE, cores = 4)
 
 out <- plyr::ldply(unique(gpdd$main_id), extract_model,
   sub_folder = id, get_phi = FALSE, type = "gompertz")
