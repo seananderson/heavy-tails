d3751f60c80c4772b3867a647b2f897461e81006
Sean Anderson <github@seananderson.ca>
2014-11-07 15:11:18 -0800

    Comment out old code

--- analysis/6-plot-eg-ts-gpdd.R
+++ analysis/6-plot-eg-ts-gpdd.R
@@ -2,11 +2,6 @@ heavy <- filter(gomp_hat_base, p10 > .5) %>%
 #   filter(fur == FALSE) %>%
   select(main_id, common_name, p10, p20, nu_5, nu_25, nu_50, nu_75, nu_95)
 
-#gpdd_heavy <- filter(gpdd, main_id %in% heavy$main_id)
-#gpdd_heavy <- filter(gpdd_heavy, !grepl("harvest", ref))
-#gpdd_heavy <- filter(gpdd_heavy, !grepl("commerce", ref))
-
-
 heavy <-
   plyr::join(heavy, as.data.frame(gpdd)) %>%
   arrange(main_id, p10) %>%
@@ -30,8 +25,8 @@ heavy_table <- plyr::ddply(heavy, "main_id", function(x) x[1,]) %>%
 
 write.csv(heavy_table, file = "heavy.csv", row.names = FALSE)
 
-p<-ggplot(subset(gpdd, assumed_log10 == TRUE), aes(series_step, log10(population_untransformed))) + geom_point() + geom_line() + facet_wrap(~label, scales = "free_x") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-ggsave("log10-assumed.pdf", width = 13, height = 13)
+#p<-ggplot(subset(gpdd, assumed_log10 == TRUE), aes(series_step, log10(population_untransformed))) + geom_point() + geom_line() + facet_wrap(~label, scales = "free_x") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+#ggsave("log10-assumed.pdf", width = 13, height = 13)
 
 zz <- file("mainids.tex", "w")
 cat(sort(unique(gpdd$main_id)), file = zz)
