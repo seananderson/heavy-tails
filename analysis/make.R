fbf96b4a345801e7c7893e2e0fbb370846bcad58
Sean Anderson <github@seananderson.ca>
2014-11-07 15:55:49 -0800

    Add more cached files and start of makefile

--- analysis/make.R
+++ analysis/make.R
@@ -1,23 +1,39 @@
 # All steps to run the analysis
 # Created in Vim with :read !ls *.R
 
-source("0-make-data.R") # takes a few minutes
+if(!file.exists("gpdd-clean.rds"))
+  source("0-make-data.R") # takes a few minutes
 # Begin: on the westgrid server
+# Compile Stan models:
 source("1-compile-models.R") # warning: takes a long time
+# Functions used in the fitting:
 source("1.5-compile-fit-function.R")
 source("1.6-extract-function.R")
-source("2-run-model-logistic.R") # warning: takes a long time
-source("2-run-models-ar1.R") # warning: takes a long time
-source("2-run-models-base-stronger-prior.R") # warning: takes a long time
-source("2-run-models-base-weaker-prior.R") # warning: takes a long time
-source("2-run-models-base.R") # warning: takes a long time
-source("2-run-models-obs-0.2.R") # warning: takes a long time
-source("2-run-models-rate.R") # warning: takes a long time
-source("2.1-get-base-mean-sd.R")
-source("3.0-test-t-sampling.R") # warning: takes a long time
+# Run main models:
+if(!file.exists("logistic-hat.rds"))
+  source("2-run-model-logistic.R") # warning: takes a long time
+if(!file.exists("gomp-ar1-hat.rds"))
+  source("2-run-models-ar1.R") # warning: takes a long time
+if(!file.exists("gomp-base-stronger-hat.rds"))
+  source("2-run-models-base-stronger-prior.R") # warning: takes a long time
+if(!file.exists("gomp-base-weaker-hat.rds"))
+  source("2-run-models-base-weaker-prior.R") # warning: takes a long time
+if(!file.exists("gomp-base-hat.rds"))
+  source("2-run-models-base.R") # warning: takes a long time
+if(!file.exists("gomp-obs-0.2-hat.rds"))
+  source("2-run-models-obs-0.2.R") # warning: takes a long time
+if(!file.exists("rate-hat.rds"))
+  source("2-run-models-rate.R") # warning: takes a long time
+if(!file.exists("gomp-base-mean-sd.rds"))
+  source("2.1-get-base-mean-sd.R")
+# Simulation testing
+if(!file.exists("sample-t-sim-check.rds"))
+  source("3.0-test-t-sampling.R") # warning: takes a long time
 source("3.1-plot-sample-test.R")
-source("3.2-get-effect-nu-seeds.R") # warning: takes a long time
-source("3.3-test-gomp-models.R") # warning: takes a long time
+if(!file.exists("nu_effective_seeds.rda"))
+  source("3.2-get-effect-nu-seeds.R") # warning: takes a long time
+if(!file.exists("check_nu.rds"))
+  source("3.3-test-gomp-models.R") # warning: takes a long time
 # End: on the westgrid server
 source("3.4-plot-test-gomp-models.R")
 source("5-shape-data.R")
