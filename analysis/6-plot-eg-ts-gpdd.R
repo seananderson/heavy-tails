b76b4294196c56fd6012d0e52d38a2a813704695
Sean Anderson <github@seananderson.ca>
2014-11-06 17:08:13 -0800

    Tweak style of heavy ts plot

--- analysis/6-plot-eg-ts-gpdd.R
+++ analysis/6-plot-eg-ts-gpdd.R
@@ -8,7 +8,7 @@ heavy <- filter(gomp_hat_base, p10 > .5) %>%
 
 
 heavy <-
-  plyr::join(heavy, gpdd) %>%
+  plyr::join(heavy, as.data.frame(gpdd)) %>%
   arrange(main_id, p10) %>%
   mutate(id_name = paste(round(p10, 2), common_name, main_id))
 
@@ -18,7 +18,11 @@ theme_set(theme_bw())
 p <- ggplot(heavy, aes(series_step, population_untransformed, colour = taxonomic_class)) + geom_line() + facet_wrap(~id_name, scales = "free")
 ggsave("ts-gpdd-heavy-eg-base.pdf", width = 15, height = 12)
 
-p <- ggplot(heavy, aes(series_step, log10(population_untransformed), colour = taxonomic_class)) + geom_line() + facet_wrap(~id_name, scales = "free", ncol = 4) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + geom_point()
+p <- ggplot(heavy, aes(decimal_year_begin, log10(population_untransformed), colour = taxonomic_class)) + geom_line() + facet_wrap(~id_name, scales = "free", ncol = 4) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + geom_point() +
+  theme(panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_blank())
 ggsave("ts-gpdd-heavy-eg-log10-base.pdf", width = 15, height = 17)
 
 heavy_table <- plyr::ddply(heavy, "main_id", function(x) x[1,]) %>%
