6a8098acbf540491a33de6891a83112f3f1ecd5d
Sean Anderson <github@seananderson.ca>
2015-06-22 20:45:39 -0700

    Add submission scripts

---
+++ analysis/sub-base-normal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#PBS -l walltime=04:00:00
+#PBS -M sean_anderson@sfu.ca
+#PBS -l pmem=1000mb
+#PBS -l procs=1
+
+module load R/3.1.1
+cd $PBS_O_WORKDIR
+R CMD BATCH --no-save 2-run-models-normal.R
