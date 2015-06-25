38bcd4db8503eea9586e4940ff2aa3791bf2063c
Sean Anderson <sean@seananderson.ca>
2015-06-24 22:40:03 -0700

    Extra skew *and* nu

--- analysis/5.10-extract-skew-samples.R
+++ analysis/5.10-extract-skew-samples.R
@@ -1,17 +1,32 @@
 library(dplyr)
 f <- list.files("~/scratch/heavy/gomp-base-skew/", pattern = "*.rds")
+set.seed(123456789)
 skew_samples <- plyr::ldply(f, function(x) {
   N <- 1000L
   message(x)
   e <- readRDS(paste0("~/scratch/heavy/gomp-base-skew/", x)) %>% rstan::extract()
-  n_samples_total <- length(e$log_skew)
-  if (n_samples_total >= N) {
-    s <- base::sample(e$log_skew, size = N, replace = FALSE)
+
+  n_samples_skew <- length(e$log_skew)
+  if (n_samples_skew >= N) {
+    s_skew <- base::sample(e$log_skew, size = N, replace = FALSE)
   } else {
-    s <- base::sample(e$log_skew, size = N, replace = TRUE)
+    s_skew <- base::sample(e$log_skew, size = N, replace = TRUE)
   }
+
+  n_samples_nu <- length(e$nu)
+  if (n_samples_nu >= N) {
+    s_nu <- base::sample(e$nu, size = N, replace = FALSE)
+  } else {
+    s_nu <- base::sample(e$nu, size = N, replace = TRUE)
+  }
+
   id <- gsub("sm-([0-9]+)\\.rds", "\\1", x) %>% as.numeric
-  data.frame(main_id = rep(id, length(s)), log_skew = s,
-    n_samples_total = rep(n_samples_total, length(s)))
+
+  data.frame(
+    main_id        = rep(id, N),
+    log_skew       = s_skew,
+    nu             = s_nu,
+    n_samples_skew = rep(n_samples_skew, N),
+    n_samples_nu   = rep(n_samples_nu, N))
 })
 saveRDS(skew_samples, file = "skew_samples.rds")
