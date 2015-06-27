2f96e54dcd5fc49227ad98b799a377b41b3fa116
Sean Anderson <sean@seananderson.ca>
2015-06-26 21:19:07 -0700

    Fixes to skew samples script

--- analysis/5.10-extract-skew-samples.R
+++ analysis/5.10-extract-skew-samples.R
@@ -1,9 +1,9 @@
 sample_posterior <- function(path = ".", N = 1000L) {
   f <- list.files(path, pattern = "*.rds")
-  samples <- plyr::ldply(f, function(x) {
+  samples <- plyr::ldply(f[1:3], function(x) {
 
     message(x)
-    e <- rstan::extract(eadRDS(paste0("~/scratch/heavy/gomp-base-skew/", x)))
+    e <- rstan::extract(readRDS(paste0(path, x)))
     n_samples <- length(e$b) # check length of an arbitrary coefficient
     id <- as.numeric(gsub("sm-([0-9]+)\\.rds", "\\1", x))
     coefs <- names(e)
@@ -19,7 +19,7 @@ sample_posterior <- function(path = ".", N = 1000L) {
 
     if ("nu" %in% coefs) {
       data.frame(out,
-        log_skew = e$log_skew[s_ids, ],
+        log_skew = e$log_skew[s_ids],
         nu       = e$nu[s_ids])
     } else {
       out
@@ -31,6 +31,6 @@ sample_posterior <- function(path = ".", N = 1000L) {
 
 set.seed(123)
 skew_samples   <- sample_posterior("~/scratch/heavy/gomp-base-skew/")
-normal_samples <- sample_posterior("~/scratch/heavy/gomp-normal/")
+normal_samples <- sample_posterior("~/scratch/heavy/gomp-base-normal/")
 saveRDS(skew_samples,   file = "skew_samples.rds")
 saveRDS(normal_samples, file = "normal_samples.rds")
