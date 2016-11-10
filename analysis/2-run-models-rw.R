731ff5fd38a472a58dcc434814b319ce3686d3e3
Sean Anderson <sean@seananderson.ca>
2016-11-10 14:44:01 -0800

    Run RW model

---
+++ analysis/2-run-models-rw.R
@@ -0,0 +1,19 @@
+# This file runs the models with a logistic growth model
+
+source("1.5-compile-fit-function.R")
+source("1.6-extract-function.R")
+
+model <- readRDS("stan-rw.rds")
+gpdd <- readRDS("gpdd-clean.rds")
+id <- "rw"
+
+stan_dat <- "list(N = nrow(x) - 1, r_obs = x$r_obs[-1], nu_rate = 0.01)"
+stan_pars <- c("nu", "sigma_proc")
+
+fit_gpdd_model(gpdd_dat = gpdd, model = model,
+  sub_folder = id, stan_dat = stan_dat, pars = stan_pars)
+
+out <- plyr::ldply(unique(gpdd$main_id), extract_model,
+  sub_folder = id, get_phi = FALSE, type = "rw")
+
+saveRDS(out, file = paste0(id, "-hat.rds"))
