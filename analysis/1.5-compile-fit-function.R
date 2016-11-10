12444db96ca06bfd97cd37e1d53423bd7f46c863
Sean Anderson <sean@seananderson.ca>
2016-11-10 14:43:04 -0800

    Tweaks for windows

--- analysis/1.5-compile-fit-function.R
+++ analysis/1.5-compile-fit-function.R
@@ -1,11 +1,11 @@
 fit_gpdd_model <- function(gpdd_dat, model, sub_folder,
-  root_folder = ".", file_prefix = "sm",
+  root_folder = getwd(), file_prefix = "sm",
   stan_dat = paste0("list(N = nrow(x), y = log(x$population_untransformed), ",
     "nu_rate = 0.01, b_lower = -1, b_upper = 2)"),
   pars = c("lambda", "sigma_proc", "nu", "b", "phi"), max_rhat_allowed = 1.05,
   min_neff_allowed = 200, iterations = 2000, max_iterations = 8000,
   iteration_increment = 2, warmup = 1000, chains = 4, overwrite = FALSE,
-  .parallel = FALSE, refresh = -1, cores = 2, thin = 1) {
+  .parallel = FALSE, refresh = -1, cores = 1L, thin = 1) {
 
   library("rstan")
 
@@ -38,7 +38,7 @@ fit_gpdd_model <- function(gpdd_dat, model, sub_folder,
           iterations <= max_iterations) {
         d <- eval(parse(text = stan_dat))
         # refresh = -1 supresses the running messages
-        sm <- sampling(model, data = d, thin = thin,
+        sm <- rstan::sampling(model, data = d, thin = thin,
           pars = pars, iter = iterations, chains = chains, warmup = warmup)
         max_rhat <- max(summary(sm)$summary[, "Rhat"])
         min_neff <- min(summary(sm)$summary[, "n_eff"])
