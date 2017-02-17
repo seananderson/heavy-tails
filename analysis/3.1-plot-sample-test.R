07b352e40f5b53ae5452526129561a2ca42d428b
Sean Anderson <sean@seananderson.ca>
2017-02-16 18:31:58 -0800

    Reduce point size in supplement

--- analysis/3.1-plot-sample-test.R
+++ analysis/3.1-plot-sample-test.R
@@ -7,7 +7,7 @@ theme_set(theme_bw())
 library("dplyr")
 
 sample_t <- filter(sample_t, nu_true > 1)
-p <- ggplot(sample_t, aes(iter, 1/med_nu)) + geom_pointrange(aes(ymin = 1/l_nu, ymax = 1/u_nu)) +
+p <- ggplot(sample_t, aes(iter, 1/med_nu)) + geom_pointrange(aes(ymin = 1/l_nu, ymax = 1/u_nu), size = 0.2) +
   facet_grid(nu_true~N) +
   scale_y_continuous(breaks = c(0, 0.1, 0.2, 0.333, 0.5),
     labels = c("Infinity", "10", "5", "3", "2")) +
@@ -16,7 +16,7 @@ p <- ggplot(sample_t, aes(iter, 1/med_nu)) + geom_pointrange(aes(ymin = 1/l_nu, 
 
   ggsave("t-dist-sampling-sim-prior-exp0point01.pdf", width = 8, height = 5)
 
-p <- ggplot(sample_t, aes(iter, med_sigma)) + geom_pointrange(aes(ymin = l_sigma, ymax = u_sigma)) +
+p <- ggplot(sample_t, aes(iter, med_sigma)) + geom_pointrange(aes(ymin = l_sigma, ymax = u_sigma), size = 0.2) +
   facet_grid(nu_true~N) +
   geom_hline(aes(yintercept = 1), col = "red") +
   xlab("Iteration") + ylab("Scale parameter")
